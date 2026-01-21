82960eea628276ea1ce6ea9d12c8a286403f22ec
rajibadhi <rajibadh@gmail.com>
2026-01-21 17:23:17 +0545

    Fix Vite sitemap plugin config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { sitemapPlugin } from 'vite-plugin-sitemap'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     react(),
-    sitemapPlugin({
-      hostname: 'https://www.rajibadhikari.com.np',
-    }),
+    sitemap({ hostname: 'https://www.rajibadhikari.com.np' }),
   ],
 })
