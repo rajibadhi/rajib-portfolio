336a45c804262b847c52e3be4f929a7fb5aef212
rajibadhi <rajibadh@gmail.com>
2026-01-21 17:21:33 +0545

    Add sitemap and robots for SEO

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { sitemapPlugin } from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemapPlugin({
+      hostname: 'https://www.rajibadhikari.com.np',
+    }),
+  ],
 })
